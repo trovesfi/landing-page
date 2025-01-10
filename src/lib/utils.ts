952c9c3a8ebeac96bd5dd28f7eac10b2c30b69cf
akiraonstarknet <zkdice@proton.me>
2025-01-10 12:20:49 +0530

    fix getHosturl [2]

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,12 @@ export function cn(...inputs: ClassValue[]) {
 
 // get domain name without subdomain
 export function getHosturl() {
-  return (typeof window != undefined ? window.location.hostname.split('.').slice(-2).join('.') : null) || 'strkfarm.com';
+  const FALLBACK = "strkfarm.com";
+  try {
+    return (typeof window != 'undefined' ? window.location.hostname.split('.').slice(-2).join('.') : null) || FALLBACK;
+  } catch (e) {
+    return FALLBACK;
+  }
 }
 
 console.log(`getHosturl()`, getHosturl());
